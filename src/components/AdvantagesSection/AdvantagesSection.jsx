ea4ba35aeb9dc207235b03df123f23bbee98be31
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-08-02 10:41:41 +0300

    Happy customerl in a Component

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,29 +1,15 @@
+import Customers from '../Customers/Customers';
 import styles from './AdvantagesSection.module.css';
 import img from './img/mobile_home_img.jpg';
-import photo1 from './img/photo1.png';
-import photo2 from './img/photo2.png';
-import photo3 from './img/photo3.png';
 
 export default function AdvantagesSection() {
-  const photos = [{ src: photo1 }, { src: photo2 }, { src: photo3 }];
   return (
     <section className={styles.AdvantagesSection}>
       <picture>
         <source srcSet={img} type="image/jpeg" />
         <img src={img} alt="girl with a bottle of water" />
       </picture>
-      <div className={styles.customersBox}>
-        <ul className={styles.customersList}>
-          {photos.map((photo, idx) => (
-            <li key={idx}>
-              <img src={photo.src} />
-            </li>
-          ))}
-        </ul>
-        <p className={styles.customersText}>
-          Our <span className={styles.customersSpan}>happy</span> customers
-        </p>
-      </div>
+      <Customers />
       <ul className={styles.advantagesList}>
         <li className={styles.advHabit}>
           <p>Habit drive</p>
---
+++ src/components/Customers/Customers.jsx
@@ -0,0 +1,23 @@
+import photo1 from '../AdvantagesSection/img/photo1.png';
+import photo2 from '../AdvantagesSection/img/photo2.png';
+import photo3 from '../AdvantagesSection/img/photo3.png';
+
+import styles from './Customers.module.css';
+
+export default function Customers() {
+  const photos = [{ src: photo1 }, { src: photo2 }, { src: photo3 }];
+  return (
+    <div className={styles.customersBox}>
+      <ul className={styles.customersList}>
+        {photos.map((photo, idx) => (
+          <li key={idx}>
+            <img src={photo.src} />
+          </li>
+        ))}
+      </ul>
+      <p className={styles.customersText}>
+        Our <span className={styles.customersSpan}>happy</span> customers
+      </p>
+    </div>
+  );
+}

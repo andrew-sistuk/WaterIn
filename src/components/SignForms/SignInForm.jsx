fa46e5019a528ea4d786b72c4d9bfcd5186110ea
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-03 17:36:29 +0300

    "Added react-hook-form and yup for form validation, updated SignInForm and SignUpForm components to use react-hook-form, and added error messages for invalid inputs."

--- src/components/SignForms/SignInForm.jsx
+++ src/components/SignForms/SignInForm.jsx
@@ -3,26 +3,53 @@ import css from './SignForms.module.css';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import { useState } from 'react';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 export default function SignInForm() {
   const [isPassOpen, setIsPassOpen] = useState(false);
 
+  const schema = yup.object().shape({
+    email: yup.string().email('Invalid email format').required('Email is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Password is required'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <form className={css['sign-form']}>
+    <form className={css['sign-form']} onSubmit={handleSubmit(onSubmit)} noValidate>
       <h2 className={css.header}>Sign In</h2>
       <label className={css.label} htmlFor="email">
         Email:
       </label>
       <input
+        {...register('email')}
         className={clsx(css.input, css.email)}
         type="email"
         placeholder="Enter your email"
         id="email"
       />
+      {errors.email && <p className={css.error}>{errors.email.message}</p>}
       <label className={css.label} htmlFor="password">
         Password:
       </label>
       <div className={css['box-pass']}>
         <input
+          {...register('password')}
           className={clsx(css.input, css.pass)}
           type={isPassOpen ? 'text' : 'password'}
           placeholder="Enter your password"
@@ -36,6 +63,7 @@ export default function SignInForm() {
           {isPassOpen ? <FiEye className={css.icon} /> : <FiEyeOff className={css.icon} />}
         </button>
       </div>
+      {errors.password && <p className={css.error}>{errors.password.message}</p>}
       <button className={css.submit} type="submit">
         Sign In
       </button>
--- src/components/SignForms/SignUpForm.jsx
+++ src/components/SignForms/SignUpForm.jsx
@@ -3,27 +3,58 @@ import css from './SignForms.module.css';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import { useState } from 'react';
 
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 export default function SignUpForm() {
   const [isPassOpen, setIsPassOpen] = useState(false);
   const [isPassRepOpen, setIsPassRepOpen] = useState(false);
 
+  const schema = yup.object().shape({
+    email: yup.string().email('Invalid email format').required('Email is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .required('Password is required'),
+    reqPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'Passwords must match')
+      .required('Repeat password is required'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <form className={css['sign-form']}>
+    <form className={css['sign-form']} onSubmit={handleSubmit(onSubmit)} noValidate>
       <h2 className={css.header}>Sign Up</h2>
       <label className={css.label} htmlFor="email">
         Email:
       </label>
       <input
+        {...register('email')}
         className={clsx(css.input, css.email)}
         type="email"
         placeholder="Enter your email"
         id="email"
       />
+      {errors.email && <p className={css.error}>{errors.email.message}</p>}
       <label className={css.label} htmlFor="password">
         Password:
       </label>
       <div className={css['box-pass']}>
         <input
+          {...register('password')}
           className={clsx(css.input, css.pass)}
           type={isPassOpen ? 'text' : 'password'}
           placeholder="Enter your password"
@@ -37,15 +68,17 @@ export default function SignUpForm() {
           {isPassOpen ? <FiEye className={css.icon} /> : <FiEyeOff className={css.icon} />}
         </button>
       </div>
-      <label className={css.label} htmlFor="password">
+      {errors.password && <p className={css.error}>{errors.password.message}</p>}
+      <label className={css.label} htmlFor="reqPassword">
         Repeat password:
       </label>
       <div className={css['box-pass']}>
         <input
+          {...register('reqPassword')}
           className={clsx(css.input, css.pass)}
           type={isPassRepOpen ? 'text' : 'password'}
           placeholder="Enter your password"
-          id="password"
+          id="reqPassword"
         />
         <button
           type="button"
@@ -55,6 +88,7 @@ export default function SignUpForm() {
           {isPassRepOpen ? <FiEye className={css.icon} /> : <FiEyeOff className={css.icon} />}
         </button>
       </div>
+      {errors.reqPassword && <p className={css.error}>{errors.reqPassword.message}</p>}
       <button className={css.submit} type="submit">
         Sign Up
       </button>

9e9efeb195902945131c6db516984e33e6269366
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-08-03 17:04:54 +0300

    Create Button.jsx

---
+++ src/components/UserPanel/Button.jsx
@@ -0,0 +1,16 @@
+import clsx from 'npm install clsx';
+import css from './Button.module.css';
+
+const buildLinkClass = classname => {
+  return clsx(css.button, classname && classname);
+};
+
+const Button = ({ children, classname, ...props }) => {
+  return (
+    <button className={buildLinkClass(classname)} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

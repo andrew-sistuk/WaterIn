bedf3d822f134ff62f458a76ecff91014086985c
Mykhailo Sydoriuk <olyaruzhylenko@gmail.com>
2024-08-03 17:47:06 +0300

    dekstop sryles поки що розміщення в колону

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -4,11 +4,14 @@ import img1x from '../../images/Advantages/mobile_home_img.jpg';
 import img2x from '../../images/Advantages/mobile_home_img_2х.jpg';
 import img1x_768 from '../../images/Advantages/tablet_home_img.jpg';
 import img2x_768 from '../../images/Advantages/tablet_home_img_2х.jpg';
+import img1x_1440 from '../../images/Advantages/dekstop_home_img.jpg';
+import img2x_1440 from '../../images/Advantages/dekstop_home_img_2х.jpg';
 
 export default function AdvantagesSection() {
   return (
     <section className={styles.advantagesSection}>
       <picture>
+      <source srcSet={`${img1x_1440} 1x, ${img2x_1440} 2x`} media="(min-width: 1440px)" type="image/jpeg" />
       <source srcSet={`${img1x_768} 1x, ${img2x_768} 2x`} media="(min-width: 768px)" type="image/jpeg" />
         <source srcSet={`${img1x} 1x, ${img2x} 2x`} type="image/jpeg" />
         <img src={img1x} alt="girl with a bottle of water" className={styles.image}/>

15b92061cbf04b52c5fb542411c345e1c4e8fae9
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-08-03 17:04:30 +0300

    Update UserPanel.jsx

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react';
+//import { useAuth } from '../../../hooks';
+import { UserBar } from '../UserBar/UserBar';
 import css from './UserPanel.module.css';
+//import { UserSettingsModal } from '../../modals/UserSettingsModal/UserSettingsModal';
+import { LogOutModal } from '../../modals/LogOutModal/LogOutModal';
 
-const UserPanel = () => {};
-export default UserPanel;
+export const UserPanel = () => {
+  const { user } = useAuth();
+  const [isActiveSettings, setIsActiveSettings] = useState(false);
+  const [isActiveLogout, setIsActiveLogout] = useState(false);
+
+  const croppName = str => {
+    if (str.length > 8) {
+      str = str.slice(0, 8) + '...';
+    }
+    return str;
+  };
+
+  return (
+    <>
+      <div className={css.userPanel}>
+        <p className={css.text}>
+          Hello, <strong>{user ? croppName(user.name) : 'Anonymous'}!</strong>
+        </p>
+        <UserBar setIsActiveSettings={setIsActiveSettings} setIsActiveLogout={setIsActiveLogout} />
+      </div>
+      <UserSettingsModal active={isActiveSettings} setActive={setIsActiveSettings} />
+      <LogOutModal active={isActiveLogout} setActive={setIsActiveLogout} />
+    </>
+  );
+};

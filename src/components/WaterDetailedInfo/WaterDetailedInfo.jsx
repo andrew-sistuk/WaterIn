18a61548c1b63562fc1300ca652ae2629f4f992f
Serhii-Yakimtso <serafyso@gmail.com>
2024-08-04 21:49:11 +0300

    add

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,16 +1,15 @@
-import css from './WaterDetailedInfo.module.css';
-
 import UserPanel from '../UserPanel/UserPanel';
 import DailyInfo from '../DailyInfo/DailyInfo';
 import MonthInfo from '../MonthInfo/MonthInfo';
 
-import Calendar from '../Calendar/Calendar';
+import css from './WaterDetailedInfo.module.css';
 
 const WaterDetailedInfo = () => {
   return (
-    <div className={css.container}>
-      <div className={css.component}>Component</div>
-      <Calendar />
+    <div className={css.wrapper}>
+      <UserPanel />
+      <DailyInfo />
+      <MonthInfo />
     </div>
   );
 };
---
+++ src/components/WaterItem/WaterItem.jsx
@@ -0,0 +1,34 @@
+import { FiEdit2 } from 'react-icons/fi';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { FaGlassWater } from 'react-icons/fa6';
+
+import css from './WaterItem.module.css';
+
+const WaterItem = () => {
+  const value = '250';
+  const time = '7:00 AM';
+
+  console.log('Замінити btnEdit та btnDelete на компонент кнопку');
+  console.log('Замінити іконки?');
+
+  return (
+    <li className={css.item}>
+      {/* <div className={css.icon}> */}
+      <FaGlassWater className={css.iconMain} size="38" />
+      {/* </div> */}
+      <div className={css.wrapperData}>
+        <p className={css.value}>{value} ml</p>
+        <p className={css.time}>{time}</p>
+      </div>
+      <div className={css.wrapperBtn}>
+        {/* <div className={css.btnEdit}> */}
+        <FiEdit2 className={css.btnIcon} size="14" />
+        {/* </div> */}
+        {/* <div className={css.btnDelete}> */}
+        <AiOutlineDelete className={css.btnIcon} size="14" />
+        {/* </div> */}
+      </div>
+    </li>
+  );
+};
+export default WaterItem;

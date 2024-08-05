50b2e4aee866107dfb3f34a9ad5dd4a88b8cb13e
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-05 09:42:32 +0300

    svg

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgSpritePlugin from 'vite-plugin-svg-sprite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+     svgSpritePlugin({
+      symbolId: 'icon-[name]', // Налаштуйте ідентифікатори символів
+    })
+  ],
   build: {
     sourcemap: true,
   }

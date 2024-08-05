2b5af39ff9233d45ff3f513e1d9e2871830aa8a2
Serhii-Yakimtso <serafyso@gmail.com>
2024-08-06 00:11:38 +0300

    edit water item

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -1,6 +1,6 @@
 import { FiEdit2 } from 'react-icons/fi';
 import { AiOutlineDelete } from 'react-icons/ai';
-// import { FaGlassWater } from 'react-icons/fa6';
+import Cap from '../../assets/icons/water-glass.svg?react';
 
 import css from './WaterItem.module.css';
 
@@ -13,25 +13,16 @@ const WaterItem = () => {
 
   return (
     <li className={css.item}>
-      {/* <div className={css.icon}> */}
-      <img className={css.iconMain} src="/waterin.svg" alt="" />
+      {/* <img className={css.iconMain} src="/waterin.svg" alt="" /> */}
+      <Cap className={css.iconMain} />
 
-      {/* <svg className={css.iconMain}>
-        <use href="/waterin.svg"></use>
-      </svg> */}
-      {/* <FaGlassWater className={css.iconMain} size="38" /> */}
-      {/* </div> */}
       <div className={css.wrapperData}>
         <p className={css.value}>{value} ml</p>
         <p className={css.time}>{time}</p>
       </div>
       <div className={css.wrapperBtn}>
-        {/* <div className={css.btnEdit}> */}
         <FiEdit2 className={css.btnIcon} size="14" />
-        {/* </div> */}
-        {/* <div className={css.btnDelete}> */}
         <AiOutlineDelete className={css.btnIcon} size="14" />
-        {/* </div> */}
       </div>
     </li>
   );

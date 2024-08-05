0d4ed5d636c29f1f436d5d7cc49b1e13f75f4897
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-05 23:27:24 +0300

    "Added react-redux hooks and fetchDates operation to WaterDetailedInfo component"

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,10 +1,19 @@
 import UserPanel from '../UserPanel/UserPanel';
 import DailyInfo from '../DailyInfo/DailyInfo';
 import MonthInfo from '../MonthInfo/MonthInfo';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchDates } from '../../redux/dates/operations';
 
 import css from './WaterDetailedInfo.module.css';
 
 const WaterDetailedInfo = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDates());
+  }, [dispatch]);
+
   return (
     <div className={css.wrapper}>
       <UserPanel />

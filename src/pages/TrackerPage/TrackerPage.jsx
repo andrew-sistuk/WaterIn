1816660125b7188ce0633f60c972cf35d654bdce
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-08-05 22:17:26 +0300

    Update TrackerPage.jsx

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,11 +1,13 @@
 import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
 import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
+import LogOutModal from '../../components/LogOutModal/LogOutModal';
 
 const TrackerPage = () => {
   return (
     <div>
       <WaterMainInfo />
       <WaterDetailedInfo />
+      <LogOutModal />
     </div>
   );
 };

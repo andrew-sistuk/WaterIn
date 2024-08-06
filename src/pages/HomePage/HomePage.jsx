b48684ddbf639d799b03d34f4be950a63f2feca0
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-08-06 13:00:08 +0300

    signIn

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,37 +2,9 @@ import WelcomeContainer from '../../components/WelcomeContainer/WelcomeContainer
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 
-/////////////////////Delete then//////////////////////////
-import { login } from '../../redux/auth/operations';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-
 export default function HomePage() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    console.log('запит на логін');
-    const userDate = {
-      email: 'deidar@gmail.com',
-      password: 'DEIDARRR',
-    };
-    dispatch(login(userDate));
-  };
-  const logIn = useSelector(selectIsLoggedIn);
-  useEffect(() => {
-    if (logIn) {
-      navigate('/tracker');
-    }
-  }, [logIn, navigate]);
-  ///////////////////////////////////////////////////////
-
   return (
     <section className="container">
-      <button onClick={handleLogin}>login</button>
-
       <WelcomeContainer>
         <WelcomeSection />
         <AdvantagesSection />

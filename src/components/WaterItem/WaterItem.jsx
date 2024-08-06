5fceb6195463da8f7879f8162c82514ae7e936b6
Serhii-Yakimtso <serafyso@gmail.com>
2024-08-06 19:48:48 +0300

    add map for item

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -4,27 +4,25 @@ import Cap from '../../assets/icons/water-glass.svg?react';
 
 import css from './WaterItem.module.css';
 
-const WaterItem = () => {
-  const value = '250';
-  const time = '7:00 AM';
+const WaterItem = ({ data }) => {
+  const { userId, volume, drinkTime } = data;
 
   // console.log('Замінити btnEdit та btnDelete на компонент кнопку');
   // console.log('Замінити іконки?');
 
   return (
-    <li className={css.item}>
-      {/* <img className={css.iconMain} src="/waterin.svg" alt="" /> */}
+    <>
       <Cap className={css.iconMain} />
 
       <div className={css.wrapperData}>
-        <p className={css.value}>{value} ml</p>
-        <p className={css.time}>{time}</p>
+        <p className={css.value}>{volume} ml</p>
+        <p className={css.time}>{drinkTime}</p>
       </div>
       <div className={css.wrapperBtn}>
         <FiEdit2 className={css.btnIcon} size="14" />
         <AiOutlineDelete className={css.btnIcon} size="14" />
       </div>
-    </li>
+    </>
   );
 };
 export default WaterItem;

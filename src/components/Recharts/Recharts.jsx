997b119ba71d985c757f6b25dd0c0a08ce3bf014
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2024-08-09 00:15:58 +0300

    grafSt

--- src/components/Recharts/Recharts.jsx
+++ src/components/Recharts/Recharts.jsx
@@ -4,29 +4,48 @@ import { selectItems } from '../../redux/dates/slice';
 import css from './Recharts.module.css';
 
 ('use client');
-import {
-  AreaChart,
-  Area,
-  ResponsiveContainer,
-  CartesianGrid,
-  // YAxis,
-  // XAxis,
-  Tooltip,
-} from 'recharts';
+import { AreaChart, Area, ResponsiveContainer, YAxis, XAxis, Tooltip } from 'recharts';
 
 const Recharts = () => {
   const dates = useSelector(selectItems);
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    // conso
+    if (active && payload && payload.length) {
+      return (
+        <div className={css.customTooltip}>
+          <p className={css.label}>{`Day: ${new Date(label).getDate()}`}</p>
+          <p className={css.intro}>{`Об'єм: ${payload[0].value}`}</p>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div className={css.container}>
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart width={500} height={400} data={dates}>
-          {/* <YAxis />
-          <XAxis /> */}
-          <Tooltip />
-
-          <CartesianGrid strokeDasharray="5 5 " />
-          <Area dataKey="dayVolume" stroke="#323f47" fill="#9be1a0" />
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="65%" stopColor="#9be1a0" stopOpacity={1} />
+              <stop offset="90%" stopColor="#f0eff4" stopOpacity={0.5} />
+            </linearGradient>
+          </defs>
+          <YAxis />
+          <XAxis
+            dataKey="date"
+            interval={Math.floor(dates.length / 7)}
+            tickFormatter={date => new Date(date).getDate()}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <Area
+            dataKey="dayVolume"
+            stroke="#87d28d"
+            fill="url(#colorUv)"
+            dot={{ r: 2, stroke: '#87d28d', strokeWidth: 2, fill: '#fff' }}
+          />
         </AreaChart>
       </ResponsiveContainer>
     </div>

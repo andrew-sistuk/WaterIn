e190d9145f0090024b6a9bbf8673b95b2d67bea7
yurii <robka4an@gmail.com>
2024-08-10 17:45:19 +0300

    add functional for add and edit water

--- src/redux/waterNote/operations.js
+++ src/redux/waterNote/operations.js
@@ -2,36 +2,39 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const addWaterNote = createAsyncThunk('addWaterNote', async (payload, thunkAPI) => {
-  console.log(payload);
-  // try {
-  //   const response = await axios.post('https://waterin-server.onrender.com/water', payload.data, {
-  //     headers: {
-  //       Authorization: `Bearer ${payload.token}`,
-  //     },
-  //   });
-  //   return await response.data;
-  // } catch (error) {
-  //   return thunkAPI.rejectWithValue(error.message);
-  // }
+  const { volume, drinkTime, token } = payload;
+  try {
+    const response = await axios.post(
+      '/water',
+      { volume, drinkTime },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return await response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });
 
 export const editWaterNote = createAsyncThunk('editWaterNote', async (payload, thunkAPI) => {
-  console.log(payload);
-  // const newWaterNote = { volume: payload.data.volume, drinkTime: payload.data.drinkTime };
-  // try {
-  //   const response = await axios.patch(
-  //     `https://waterin-server.onrender.com/water/${payload.data._id}`,
-  //     newWaterNote,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${payload.token}`,
-  //       },
-  //     }
-  //   );
-  //   return await response.data;
-  // } catch (error) {
-  //   return thunkAPI.rejectWithValue(error.message);
-  // }
+  const { volume, drinkTime, _id, token } = payload;
+  try {
+    const response = await axios.patch(
+      `/water/${_id}`,
+      { volume, drinkTime },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return await response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });
 export const deleteWaterNote = createAsyncThunk('deleteWaterNote', async (payload, thunkAPI) => {
   const { modalId, token } = payload;

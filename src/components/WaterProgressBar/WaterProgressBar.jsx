804b708d80e8999b842eeb6830cad8c738c85e8d
Serhii-Yakimtso <serafyso@gmail.com>
2024-08-12 01:18:52 +0300

    fix day progres bar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,11 +1,76 @@
+import { useSelector } from 'react-redux';
+import { selectItemsDay } from '../../redux/changeDay/changeDay';
+import { selectItems } from '../../redux/dates/selectors';
+
 import css from './WaterProgressBar.module.css';
 
 const WaterProgressBar = () => {
-  const dailyNorma = 1.5;
-  const totalDrink = 0.85;
-  const percentValuedrink = Math.floor((totalDrink / dailyNorma) * 100);
+  // const parcentDate = useSelector(selectItems);
 
-  // Функція для заборони перетину максимально допустимого
+  const currentDate = useSelector(selectItemsDay);
+
+  // const getParcentForDate = (date, type = 'parcent') => {
+  //   const yearCalendar = date.getFullYear();
+  //   const monthCalendar = date.getMonth() + 1;
+  //   const dayCalendar = date.getDate();
+
+  //   const entry = parcentDate.find(item => {
+  //     const createdAtDate = new Date(item.date);
+
+  //     return (
+  //       createdAtDate.getFullYear() === yearCalendar &&
+  //       createdAtDate.getMonth() + 1 === monthCalendar &&
+  //       createdAtDate.getDate() === dayCalendar
+  //     );
+  //   });
+
+  //   if (entry) {
+  //     if (type === 'parcent') {
+  //       return `${entry.percent}%`;
+  //     } else if (type === 'id') {
+  //       return entry.id;
+  //     }
+  //   }
+
+  //   return type === 'parcent' ? '0%' : null;
+  // };
+
+  const dailyNorma = 1.5;
+  const totalDrink = 1.48;
+
+  let percentValueDrink = Math.floor((totalDrink / dailyNorma) * 100);
+
+  function getCurrentDay(currentDate) {
+    const toDay = new Date();
+    const toDayMilisekond = toDay.getTime();
+
+    const isToday = day => {
+      return (
+        new Date(day).getFullYear() === toDay.getFullYear() &&
+        new Date(day).getMonth() === toDay.getMonth() &&
+        new Date(day).getDate() === toDay.getDate()
+      );
+    };
+
+    const fullDay = `${new Date(currentDate).getDate()}.${
+      new Date(currentDate).getMonth() + 1
+    }.${new Date(currentDate).getFullYear()}`;
+
+    return isToday(toDayMilisekond) === isToday(currentDate) ? 'Today' : fullDay;
+  }
+
+  function getPercentValueDrink(value) {
+    let percentValue = value;
+
+    if (percentValue > 100) {
+      percentValue = 100;
+
+      return percentValue;
+    }
+
+    return percentValue;
+  }
+
   function fixWidthProgressBar(value) {
     if (value > 100) {
       value = 100;
@@ -22,86 +87,99 @@ const WaterProgressBar = () => {
     return value;
   }
 
-  function removeHiddenSignatureEmptyValue(value) {
-    let hidden = 'inherit';
+  function removeDisplaySignatureEmptyValue(value) {
+    let display = 'block';
     if (value < 15) {
-      hidden = 'transparent';
+      display = 'none';
 
-      return hidden;
+      return display;
     }
 
-    return hidden;
+    return display;
   }
 
-  function removeHiddenSignatureMiddleValue(value) {
-    let hidden = 'inherit';
-    if (value >= 36 && value < 60) {
-      hidden = 'transparent';
+  function removeDisplaySignatureMiddleValue(value) {
+    let display = 'block';
+    if (value >= 36 && value < 64) {
+      display = 'none';
 
-      return hidden;
+      return display;
     }
 
-    return hidden;
+    return display;
   }
 
-  function removeHiddenSignatureFullValue(value) {
-    let hidden = 'inherit';
+  function removeDisplaySignatureFullValue(value) {
+    let display = 'block';
     if (value > 80) {
-      hidden = 'transparent';
+      display = 'none';
 
-      return hidden;
+      return display;
     }
 
-    return hidden;
+    return display;
   }
 
   return (
     <div className={css.wrapper}>
-      <p className={css.title}>Today</p>
+      <p className={css.title}>{getCurrentDay(currentDate)}</p>
       <div className={css.barWrapper}>
         <div className={css.mainBar}></div>
         <div
           className={css.progressBar}
           style={{
-            width: `${fixWidthProgressBar(percentValuedrink)}%`,
+            width: `${fixWidthProgressBar(getPercentValueDrink(percentValueDrink))}%`,
           }}
         ></div>
         <div
           className={css.currentPoint}
           style={{
-            left: `${fixWidthProgressBar(percentValuedrink)}%`,
+            left: `${fixWidthProgressBar(getPercentValueDrink(percentValueDrink))}%`,
           }}
         ></div>
 
         <div className={css.signature}>
           <p
+            className={css.emptyValue}
             style={{
-              color: `${removeHiddenSignatureEmptyValue(percentValuedrink)}`,
+              display: `${removeDisplaySignatureEmptyValue(
+                getPercentValueDrink(percentValueDrink)
+              )}`,
             }}
           >
             0%
           </p>
           <p
+            className={css.middleValue}
             style={{
-              color: `${removeHiddenSignatureMiddleValue(percentValuedrink)}`,
+              display: `${removeDisplaySignatureMiddleValue(
+                getPercentValueDrink(percentValueDrink)
+              )}`,
             }}
           >
             50%
           </p>
           <p
+            className={css.fullValue}
             style={{
-              color: `${removeHiddenSignatureFullValue(percentValuedrink)}`,
+              display: `${removeDisplaySignatureFullValue(
+                getPercentValueDrink(percentValueDrink)
+              )}`,
             }}
           >
             100%
           </p>
           <p
-            className={css.current}
+            className={css.currentValue}
             style={{
-              left: `${percentValuedrink}%`,
+              left: `${
+                getPercentValueDrink(percentValueDrink) < 96
+                  ? getPercentValueDrink(percentValueDrink)
+                  : 96
+              }%`,
             }}
           >
-            {percentValuedrink}%
+            {getPercentValueDrink(percentValueDrink)}%
           </p>
         </div>
       </div>

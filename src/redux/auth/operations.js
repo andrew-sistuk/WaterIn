a7e9e444ba5d5c7c90e49663d624cfc44c7e1973
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-11 18:54:25 +0300

    merge

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -48,48 +48,59 @@ export const logout = createAsyncThunk('users/logout', async (_, thunkAPI) => {
 });
 
 ////////////////////////////////////////////////////
-export const refreshFunction = createAsyncThunk('users/refresh-user', async (_, thunkAPI) => {
-  try {
-    const response = await api.post('/users/refresh-user');
-    setAuthHeader(response.data.data.accessToken);
+export const refreshFunction = createAsyncThunk(
+  'users/refresh-user',
+  async (_, thunkAPI) => {
+    try {
+      const response = await api.post('/users/refresh-user');
+      setAuthHeader(response.data.data.accessToken);
 
-    return response.data.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+      return response.data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+  {
+    condition(_, thunkAPI) {
+      const reduxState = thunkAPI.getState();
+      const token = reduxState.auth.token;
+
+      return token !== null;
+    },
   }
-})
+);
 
 //////////////////////////////////////////
 const logoutUser = async () => {
-  await logout()
+  await logout();
   window.location.reload();
-}
+};
 
 api.interceptors.response.use(
-  (response) => {
-    if(response.status === 409) {
-      console.log('start logout')
+  response => {
+    if (response.status === 409) {
+      console.log('start logout');
     }
-    
+
     return response;
   },
-  (error) => {
-    if (error.response.status === 409){
-      logoutUser()
+  error => {
+    if (error.response.status === 409) {
+      logoutUser();
     }
     return Promise.reject(error);
   }
-)
+);
 /////////////////////////////////////////////////
 export const refresh = async () => {
   try {
     const response = await api.post('/users/refresh');
-    setAuthHeader(response.data.data.accessToken)
-    return response.data.data.accessToken
+    setAuthHeader(response.data.data.accessToken);
+    return response.data.data.accessToken;
   } catch (error) {
     console.log(error.message);
   }
- }
+};
 //////////////////////////////////////////////////
 
 export const getUser = createAsyncThunk('users/', async (userId, thunkAPI) => {

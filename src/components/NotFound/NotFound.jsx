b69d645cb00114ad3a78683602b559fbe4518a0e
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-08-12 11:13:02 +0300

    "Removed 404-1.png to 404-6.png images, updated NotFound component to use 404.png, removed unused code and updated CSS for image styling"

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,42 +1,6 @@
-import Loader from '../Loader/Loader';
 import css from './NotFound.module.css';
-import MainButton from '../../components/MainButton/MainButton';
-import PageNotFound from '/src/assets/img/404-4.png';
+import PageNotFound from '/src/assets/img/404.png';
 
 export default function NotFound() {
-  return (
-    <div>
-      <img className={css.image} src={PageNotFound} alt="Page not found" />
-      {/* <Loader /> */}
-      {/* <MainButton text="Save" onClick={() => alert('Save Button clicked!')} />
-      <MainButton text="Try tracker" onClick={() => alert('Try tracker Button clicked!')} />
-      <MainButton
-        text="Try tracker"
-        onClick={() => alert('Try tracker Button clicked!')}
-        disabled
-      />
-      <MainButton
-        text="Add water"
-        onClick={() => alert('Add water Button clicked!')}
-        icon={
-          <svg width={20} height={20} className={css.icon}>
-            <use href="/src/img/icons/sprite.svg#icon-plus"></use>
-          </svg>
-        }
-      />
-      <MainButton
-        text="Add water"
-        onClick={() => alert('Add water Button clicked!')}
-        icon={
-          <svg width={20} height={20} className={css.icon}>
-            <use href="/src/img/icons/sprite.svg#icon-plus"></use>
-          </svg>
-        }
-        iconOnly
-      />
-      <MainButton text="Cancel" onClick={() => alert('Cancel Button clicked!')} />
-      <MainButton text="Cancel" onClick={() => alert('Cancel Button clicked!')} disabled />
-      <MainButton text="Delete" onClick={() => alert('Delete Button clicked!')} /> */}
-    </div>
-  );
+  return <img className={css.image} src={PageNotFound} alt="Page not found" />;
 }

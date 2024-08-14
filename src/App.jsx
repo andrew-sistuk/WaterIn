aa21de78691834dd12ef567da7cd057e7056c2de
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-08-14 13:58:15 +0200

    tour fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }, [dispatch]);
 
   return isRefreshing ? (
-    <Loader></Loader>
+    <Loader />
   ) : (
     <Suspense fallback={<Loader />}>
       <Routes>
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -5,13 +5,13 @@ import Message from '../../components/Message/Message';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '../../redux/auth/operations.js';
-import { selectLoading, selectUser } from '../../redux/auth/selectors.js';
-import Loader from '../../components/Loader/Loader.jsx';
+import { selectUser } from '../../redux/auth/selectors.js';
+
+import { ButtonTour } from '../../components/ButtonTour/ButtonTour.jsx';
 
 const TrackerPage = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
 
   useEffect(() => {
     dispatch(getUser(user.id));
@@ -19,19 +19,16 @@ const TrackerPage = () => {
 
   return (
     <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <section className="container">
-          <>
-            <WelcomeContainer>
-              <WaterMainInfo />
-              <WaterDetailedInfo />
-            </WelcomeContainer>
-            <Message />
-          </>
-        </section>
-      )}
+      <section className="container">
+        <>
+          <ButtonTour />
+          <WelcomeContainer>
+            <WaterMainInfo />
+            <WaterDetailedInfo />
+          </WelcomeContainer>
+          <Message />
+        </>
+      </section>
     </>
   );
 };

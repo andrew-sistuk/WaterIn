c1eac54bd04712158aed6743fefb217410bf61b0
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-08-14 17:55:37 +0200

    images and fonts

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import css from './NotFound.module.css';
-import PageNotFound from '/src/assets/img/404.png';
+import PageNotFound from '/src/assets/img/404.webp';
 
 export default function NotFound() {
   return <img className={css.image} src={PageNotFound} alt="Page not found" />;

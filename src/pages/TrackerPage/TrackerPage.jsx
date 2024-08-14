81684b76e1e5e153046a2045c1ab7b9c313eddfa
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-08-14 06:52:11 +0200

    курва харе конфлікти по кругу кидати

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -2,24 +2,16 @@ import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
 import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
 import WelcomeContainer from '../../components/WelcomeContainer/WelcomeContainer';
 import Message from '../../components/Message/Message';
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { getUser } from '../../redux/auth/operations.js';
-
-
-
-
-
-import { selectUser } from '../../redux/auth/selectors.js';
-
-// import css from './TrackerPage.module.css';
-import TourButton from '../../components/ButtonTour/ButtonTour.jsx';
+import { selectLoading, selectUser } from '../../redux/auth/selectors.js';
+import Loader from '../../components/Loader/Loader.jsx';
 
 const TrackerPage = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
 
   useEffect(() => {
     dispatch(getUser(user.id));
@@ -27,15 +19,19 @@ const TrackerPage = () => {
 
   return (
     <>
-      <section className="container">
-        <TourButton />
-
-        <WelcomeContainer>
-          <WaterMainInfo />
-          <WaterDetailedInfo />
-          <Message />
-        </WelcomeContainer>
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="container">
+          <>
+            <WelcomeContainer>
+              <WaterMainInfo />
+              <WaterDetailedInfo />
+            </WelcomeContainer>
+            <Message />
+          </>
+        </section>
+      )}
     </>
   );
 };
